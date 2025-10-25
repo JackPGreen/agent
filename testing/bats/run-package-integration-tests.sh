244d00e4c1473c0936b190a2feb110c7c2278dc1
Pat <pat@fluent.do>
2025-10-25 13:19:17 +0100

    ci: support testing all package targets (#67)
    * ci: support testing all package targets
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * ci: add Ubuntu package tests
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * ci: update installation
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * ci: fix matrix generation
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * fix: add helper function for package tests
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * fix: remove unsupported rush downloads
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * fix: incorrect names
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * fix: resolve test failure for package testing
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * fix: resolve test failure for package testing
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * fix: exclude unsupported CentOS versions and also fix up binary names
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    * ci: drop ARM64 PR builds due to slower runners
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>
    
    ---------
    
    Signed-off-by: Patrick Stephens <pat@fluent.do>

--- testing/bats/run-package-integration-tests.sh
+++ testing/bats/run-package-integration-tests.sh
@@ -13,28 +13,7 @@ while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
-export CONTAINER_RUNTIME=${CONTAINER_RUNTIME:-docker}
-export BASE_IMAGE="dokken/almalinux-8"
-export DISTRO="almalinux/8"
-export FLUENT_BIT_BINARY=${FLUENT_BIT_BINARY:-/opt/fluent-bit/bin/fluent-bit}
+export FLUENT_BIT_BINARY=${FLUENT_BIT_BINARY:-/opt/fluentdo-agent/bin/fluent-bit}
 
-DOWNLOADS_DIR=${DOWNLOADS_DIR:-/tmp/downloads}
-mkdir -p "$DOWNLOADS_DIR"
-
-echo "INFO: Ensure package to use is present in $DOWNLOADS_DIR"
-echo "INFO: e.g. cd $DOWNLOADS_DIR && curl -sSfLO https://packages.fluent.do/25.10.3/output/package-almalinux-8/fluentdo-agent-25.10.3-1.x86_64.rpm"
-
-echo "INFO: building test container 'bats/test/$DISTRO'"
-"${CONTAINER_RUNTIME}" build -t "bats/test/$DISTRO" \
-	--build-arg BASE_BUILDER="$BASE_IMAGE" \
-	-f "$SCRIPT_DIR/../Dockerfile.bats" \
-	--target=test \
-	"$SCRIPT_DIR/../../"
-
-echo "INFO: running test container 'bats/test/$DISTRO'"
-"${CONTAINER_RUNTIME}" run --rm -t \
-	-v "$DOWNLOADS_DIR:/downloads:ro" \
-	-e FLUENT_BIT_BINARY="$FLUENT_BIT_BINARY" \
-	"bats/test/$DISTRO"
-
+"$SCRIPT_DIR"/run-bats.sh --filter-tags 'integration,!container,!k8s' --recursive "$SCRIPT_DIR/tests"
 echo "INFO: All tests complete"
